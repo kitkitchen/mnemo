5c5e115a756b1f3e95dc3793b3a2899999fb47f6
Sean Burman <sean.burman@icloud.com>
2024-01-16 19:20:49 +0000

    Added error handler with logger

--- errors.go
+++ errors.go
@@ -1,31 +1,62 @@
 package mnemo
 
+import (
+	"fmt"
+)
+
 type (
-	MnemoError struct {
-		Err error
-	}
-	ConnError struct {
-		StatusCode int
-		Err        error
-	}
-	RequestError struct {
-		StatusCode int
-		Err        error
-	}
-	StoreError struct {
-		Err error
+	Error[T any] struct {
+		Err    error
+		Status int
+		Logger Logger
+		level  LogLevel
 	}
 )
 
-func (e MnemoError) Error() string {
-	return "Mnemo Error: " + e.Err.Error()
+func NewError[T any](msg string, opts ...Opt[Error[T]]) Error[T] {
+	e := Error[T]{
+		Err:    fmt.Errorf(msg),
+		Logger: logger,
+	}
+	return e
 }
-func (e ConnError) Error() string {
-	return "Mnemo Conn Error: " + e.Err.Error()
+
+func (e Error[T]) Error() string {
+	return fmt.Sprintf("%v error: %v", new(T), e.Err.Error())
 }
-func (e RequestError) Error() string {
-	return "Mnemo Request Error: " + e.Err.Error()
+
+func (e Error[T]) IsStatusError() bool {
+	return e.Status != 0
 }
-func (e StoreError) Error() string {
-	return "Mnemo Store Error: " + e.Err.Error()
+
+func (e Error[T]) Log() {
+	switch e.level {
+	case Debug:
+		e.Logger.Debug(e.Err.Error())
+	case Info:
+		e.Logger.Info(e.Err.Error())
+	case Warn:
+		e.Logger.Warn(e.Err.Error())
+	case Fatal:
+		e.Logger.Fatal(e.Err.Error())
+	case Panic:
+		panic(e.Err.Error())
+	default:
+		e.Logger.Error(e.Err.Error())
+	}
+}
+
+func (e Error[T]) WithStatus(status int) Error[T] {
+	e.Status = status
+	return e
+}
+
+func (e Error[T]) WithLogLevel(level LogLevel) Error[T] {
+	e.level = level
+	return e
+}
+
+func IsErrorType[T any](err error) bool {
+	_, ok := err.(Error[T])
+	return ok
 }
